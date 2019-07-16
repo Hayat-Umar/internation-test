ed908b99704187ebb74e3c9bfa7bef1515938db1
Muhammad Umar Hayat <umar@creative-dots.com>
2019-07-16 17:08:05 +0500

    Passport package installed & migrations generated.

--- database/migrations/2019_07_16_114910_create_group_users_table.php
+++ database/migrations/2019_07_16_114910_create_group_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateGroupUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('group_users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->enum('user_type', ['admin', 'user']);
+            $table->integer('group_id');
+            $table->integer('user_id');
+            $table->enum('membership_status', ['pending', 'active', 'revoked']);
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('group_users');
     }
 }
